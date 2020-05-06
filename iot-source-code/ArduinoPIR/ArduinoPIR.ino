8ee29ec50671d9f4d6c3783f499bdf24ec0faabd
kai618 <vanhieu1601@gmail.com>
2020-05-06 11:46:27 +0700

    added small board diagram image

---
+++ iot-source-code/ArduinoPIR/ArduinoPIR.ino
@@ -0,0 +1,24 @@
+int ledPin = 2;
+int inputPin = 3;
+char nowMode = 'x';
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  observeMotion();
+  Serial.println(nowMode);
+  delay(500);
+}
+
+void observeMotion() {
+  if (digitalRead(inputPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    nowMode = '6';
+  } else {
+    digitalWrite(ledPin, LOW);
+    nowMode = '5';
+  }
+}
